bb76ff255f32a30462c1236ae9860f5092f5c0a6
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-02-11 13:48:30 +0000

    Update Eightball.js

--- Eightball.js
+++ Eightball.js
@@ -15,25 +15,25 @@ switch(randomNumber) {
     eightBall = 'It is certain';
     break;
   case 1:
-    eightball = 'It is decidedly so';
+    eightBall = 'It is decidedly so';
     break;
   case 2:
     eightBall = 'Perhaps';
     break;
   case 3:
-    eightball = 'Reply hazy try again';
+    eightBall = 'Reply hazy try again';
     break;
       case 4:
     eightBall = 'Cannot predict now';
     break;
   case 5:
-    eightball = 'Do not count on it';
+    eightBall = 'Do not count on it';
     break;
       case 6:
     eightBall = 'My sources say no';
     break;
   case 7:
-    eightball = 'Outlook not so good';
+    eightBall = 'Outlook not so good';
 	 break;
 }
 console.log(`The eight ball answered: ${eightBall}`);
